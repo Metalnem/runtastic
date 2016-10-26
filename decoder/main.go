5e89ae01e45edab247f7eb8c03e6005dd50c45b8
Metalnem <metalnem@mijailovic.net>
2016-10-26 18:41:49 +0200

    Decoding base64 encoded activity to its binary representation

--- decoder/main.go
+++ decoder/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
 func main() {
+	raw, err := ioutil.ReadFile("activity.dat")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encoded := strings.Split(string(raw), "\\n")
+	var decoded []byte
+
+	for _, line := range encoded {
+		b, err := base64.StdEncoding.DecodeString(line)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		decoded = append(decoded, b...)
+	}
+
+	fmt.Println(decoded)
 }
